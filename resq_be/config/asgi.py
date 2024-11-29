56ff05193f32aa60c67d6bfaf08d35f1e5604108
1010011oule <139556797+1010011oule@users.noreply.github.com>
2024-11-29 16:17:42 +0900

    Adding voice consumer and AI integration with VAD and STT

--- resq_be/config/asgi.py
+++ resq_be/config/asgi.py
@@ -1,20 +1,17 @@
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from resq_be.core.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from resq_be.consumers import VoiceConsumer
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resq_be.config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter([
+            path("ws/voice/", VoiceConsumer.as_asgi()),
+        ])
     ),
 })
-
--- resq_be/core/routing.py
+++ resq_be/core/routing.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import consumers
+from resq_be.consumers import VoiceConsumer
+
 
 websocket_urlpatterns = [
     path('ws/voice/', consumers.ChatConsumer.as_asgi()),
     path('ws/chat/', consumers.ChatConsumer.as_asgi()),
+    path("ws/voice/", VoiceConsumer.as_asgi()),
 ]
