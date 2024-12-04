129279c3c55ac81535d4af859e0dd3abef7216e8
JJ <149689243+jayjaychoi1@users.noreply.github.com>
2024-12-05 06:34:13 +0900

    Merged Two Sources
    Sending and fetching message from server will be handled using one source code for simplicity!

--- resq_ios/resq_ios/MessageServer.swift
+++ resq_ios/resq_ios/MessageServer.swift
@@ -1,8 +1,44 @@
+//
+//  MessageServer.swift
+//  resq_ios
+//
+//  Created by JJ Choi on 11/1/24.
+//
+
 import Foundation
 
-class MessageSender {
-    static let shared = MessageSender()
+class MessageServer {
+    static let shared = MessageServer()
     
+    private init() {} // Prevent instantiation
+    
+    //Fetch Messages
+    func fetchMessages(for callID: Int, completion: @escaping (Result<[String], Error>) -> Void) {
+        let url = URL(string: "https://your-api-endpoint.com/getMessages?callID=\(callID)")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let messages = try JSONDecoder().decode([String].self, from: data)
+                completion(.success(messages))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    //Send Messages
     func sendMessage(to callID: Int, message: String, completion: @escaping (Result<String, Error>) -> Void) {
         let url = URL(string: "https://your-api-endpoint.com/sendMessage")!
         var request = URLRequest(url: url)
